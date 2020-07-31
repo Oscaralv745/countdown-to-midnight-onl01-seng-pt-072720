3b7686e94a2f973e01e96632d6c13663e88fabec
Oscar Alvarez <gavinalv6@gmail.com>
2020-07-31 15:39:55 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 #write your code here
 number = 10
 def countdown
-  while number -= 0 
-    return "#{number} SECOND(S)!"
+  while number > 0 
+    puts "#{number} SECOND(S)!"
     number -= 1
   end
   return "HAPPY NEW YEAR!"
